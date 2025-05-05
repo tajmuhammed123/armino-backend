0caa4b47f466eee7ccda9ca7d85b97c8b2af6ecc
Taj Muhammed <44499013+tajmuhammed123@users.noreply.github.com>
2025-05-06 03:45:24 +0530

    Update weatherAPI.js

--- utils/weatherAPI.js
+++ utils/weatherAPI.js
@@ -16,12 +16,7 @@ const fetchWeatherData = async (city) => {
       city: data.name,
       temperature: data.main.temp,
       description: data.weather[0].description,
-      icon: data.weather[0].icon,
       date: new Date().toISOString().split("T")[0],
-      feels_like: data.main.feels_like,
-      sunset: new Date(data.sys.sunset * 1000).toLocaleTimeString(),
-      humidity: data.main.humidity,
-      wind_speed: data.wind.speed,
     };
   } catch (error) {
       throw new Error(`Failed to fetch weather data: ${error.message}`);
